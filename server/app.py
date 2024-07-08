394aa77ad49c31c5e21c021217e4c3b5c29347f3
Njagi Githaiga <njagigithaiga@gmail.com>
2024-07-08 22:50:13 +0300

    Submit code challenge

--- server/app.py
+++ server/app.py
@@ -13,7 +13,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 
 db.init_app(app)
 
@@ -23,6 +23,88 @@ api = Api(app)
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+class Restaurants_Resource(Resource):
+    def get(self):
+        restaurants = [restaurant.to_dict(only=("id", "name", "address")) for restaurant in Restaurant.query.all()]
+        response = make_response(restaurants, 200)
+
+        return response
+    
+class Single_Restaurant_Resource(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+
+        if restaurant:
+            rest_dict = restaurant.to_dict()
+            response = make_response(rest_dict, 200)
+        else:
+            response = make_response({"error": "Restaurant not found"}, 404)
+
+        return response
+
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            response = make_response({"message": "Restaurant deleted"}, 204)
+        else:
+            response = make_response({"error": "Restaurant not found"}, 404)
+            
+        return response
+
+class Pizzas_Resource(Resource):
+    def get(self):
+        pizzas = [pizza.to_dict(only=("id", "name", "ingredients")) for pizza in Pizza.query.all()]
+
+        response = make_response(pizzas, 200)
+
+        return response
+
+class RestaurantPizzas_Resource(Resource):
+    def post(self):
+        data = request.get_json()
+
+        try:
+            restaurant_id = data['restaurant_id']
+            pizza_id = data['pizza_id']
+            price = data['price']
+
+            if price < 1 or price > 30:
+                response = make_response({"errors": ["validation errors"]}, 400)
+
+                return response
+
+            # Create a new instance of RestaurantPizza
+            new_restaurant_pizza = RestaurantPizza(
+                price=price,
+                pizza_id=pizza_id,
+                restaurant_id=restaurant_id
+            )
+            db.session.add(new_restaurant_pizza)
+            db.session.commit()
+
+            pizza_dict = new_restaurant_pizza.to_dict()
+
+            response = make_response(pizza_dict, 201)
+            
+            return response
+
+        # Handle errors
+        except KeyError as e:
+            return make_response({"errors": [f"Missing key: {str(e)}"]}, 400)
+        except Exception as e:
+            db.session.rollback()
+            return make_response({"errors": [str(e)]}, 400)
+        finally:
+            db.session.close()
+
+api.add_resource(Restaurants_Resource, "/restaurants")
+api.add_resource(Single_Restaurant_Resource, "/restaurants/<int:id>")
+api.add_resource(Pizzas_Resource, "/pizzas")
+api.add_resource(RestaurantPizzas_Resource, "/restaurant_pizzas")
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -21,8 +21,13 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="restaurant", cascade="all, delete-orphan")
+
+     # association proxy
+    pizzas = association_proxy("restaurant_pizzas", "pizza", creator=lambda pizza_obj: RestaurantPizza(pizza=pizza_obj))
 
     # add serialization rules
+    serialize_rules = ("-pizzas",)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +41,13 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="pizza", cascade="all, delete-orphan")
+
+    # association proxy
+    restaurants = association_proxy("restaurant_pizzas", "restaurant", creator=lambda restaurant_obj: RestaurantPizza(restaurant=restaurant_obj))
 
     # add serialization rules
+    serialize_rules = ("-restaurants",)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -49,11 +59,25 @@ class RestaurantPizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
+    # add foreign keys
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+
     # add relationships
+    pizza = db.relationship("Pizza", back_populates="restaurant_pizzas")
+
+    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
 
     # add serialization rules
+    serialize_rules = ("-pizza.restaurant_pizzas", "-restaurant.restaurant_pizzas",)
 
     # add validation
+    @validates("price")
+    def validate_price(self, key, price):
+        if price < 1 or price > 30:
+            raise ValueError("price must be between 1 and 30")
+        return price
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
